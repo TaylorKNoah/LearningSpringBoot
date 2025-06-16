647e06fa548b4dee1bd8aee36df83e74ee6aeaaa
Taylor <taylorknoah@gmail.com>
2025-06-16 15:32:23 -0700

    bugfix/address_table_col_name_for_hibernation_use

--- springboot-exercises/src/main/resources/db/migration/V1__initial_migration.sql
+++ springboot-exercises/src/main/resources/db/migration/V1__initial_migration.sql
@@ -8,11 +8,11 @@ CREATE TABLE `users` (
 
 CREATE TABLE `addresses` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `userId` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
   `street` varchar(255) NOT NULL,
   `city` varchar(255) NOT NULL,
   `zip` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_address_user_idx` (`userId`),
-  CONSTRAINT `fk_address_user` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+  KEY `fk_address_user_idx` (`user_id`),
+  CONSTRAINT `fk_address_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
